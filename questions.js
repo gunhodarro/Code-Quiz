d51c4f1d2fe139bba69ee5f442829591bb8e7d31
matthew juretic <gunhodarro@gmail.com>
2019-10-05 14:19:01 -0600

    Questions added to Quiz

---
+++ questions.js
@@ -0,0 +1,43 @@
+// Create Questions for quiz
+var allQuestions = [{
+    question: "What is  a binary variable, having two possible values called “true” and “false.”.?",
+    options: ["String", "Boolean", "Number", "object"],
+    answer: 1
+  }, {
+    question: "A ______ can be any text inside double or single quotes",
+    options: ["String", "Boolean", "Number", "Object"],
+    answer: 0
+  }, {
+    question: "_______ can be written with or without decimals.",
+    options: ["Boolean", "String", "function","Numbers"],
+    answer: 3
+  },{
+    question: "A JavaScript ______ is a block of code designed to perform a particular task?",
+    options: ["Object", "Function", "String", "Boolean"],
+    answer: 1
+  }, {
+    question: "JavaScript ______ are used to store multiple values in a single variable.?",
+    options: ["Functions", "Numbers", "Arrays", "Booleans"],
+    answer: 2
+  }
+];
+ 
+
+
+//Create a for loop to go through the questions
+function startGame(){ 
+  for (i = 0; i < allQuestions.length; i++) {
+    allQuestions[0].question;
+    allQuestions[1].question;
+    allQuestions[2].question;
+    allQuestions[3].question;
+    allQuestions[4].question;
+   }
+   }
+  
+  console.log(allQuestions[0].question);
+  console.log(allQuestions[1].question);
+  console.log(allQuestions[2].question);
+  console.log(allQuestions[3].question);
+  console.log(allQuestions[4].question);
+ 
